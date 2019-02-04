62dd56145fd35448b55c2c096402dc1825c8bb2c
Kronos <42975620+SimonTConrad@users.noreply.github.com>
2019-02-04 12:40:25 -0500

    Add files via upload

---
+++ src/Rectangle.java
@@ -0,0 +1,20 @@
+
+public class Rectangle 
+{
+
+	
+	
+		//fields for dimensions only
+		
+		// methods
+		// setWidth
+		// setHeight
+		// getWidth
+		// getHeight
+		// getArea
+		// getDiagonal
+		
+
+	
+
+}
